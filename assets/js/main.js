0680923b1e1a2067b2fc5c6a12a6209148afd7d5
Anjali <anjalikumari13617@gmail.com>
2021-01-23 20:08:52 +0530

    edited links to side nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,11 +63,11 @@ let header = $(`
 <!--Side Nav Bar -->
 <ul class="sidenav" id="mobile-demo">
 <li><a href="index.html">Home</a></li>
-<li><a href="experience.html">Experience</a></li>
+<li><a href="internship.html">Internship</a></li>
 <li><a href="projects.html">Projects</a></li>
-<li><a href="research.html">Research</a></li>
+<li><a href=achievements.html">Achievement</a></li>
 <li><a href="education.html">Education</a></li>
-<li><a href="index.html">Blog</a></li>
+<li><a href="https://anjali1361.github.io/my_blog">Blog</a></li>
 <li><a href="https://drive.google.com/file/d/1Dd0PPo9b7HjrbUh2_krERFEnR4WkQqxk/view?usp=drivesdk" target="_blank">My Resume</a></li>
 
 
