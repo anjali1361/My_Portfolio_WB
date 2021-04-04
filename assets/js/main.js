0f3aaf49b83c5b7032ef860cb57fd51901d82e35
Anjali <anjalikumari13617@gmail.com>
2021-04-04 20:59:29 +0530

    edited resume link

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,7 @@ let header = $(`
         <li><a class="white-text" href="achievements.html">Achievement</a></li>
         <li><a class="white-text" href="education.html">Education</a></li>
         <li><a class="white-text" href="https://anjali1361.github.io/my_blog" target="">Blog</a></li>
-        <li><a class="white-text" href="https://drive.google.com/file/d/1LCpxqslcsgiKYXXmkfGSm72VEuzEaWrc/view?usp=sharing" target="_blank">My Resume</a></li>
+        <li><a class="white-text" href="https://drive.google.com/file/d/1Kv2u4xuixG4V1ruiVdu1ujy3MrM558SM/view?usp=sharing" target="_blank">My Resume</a></li>
         
         <!--TOGGLE FOR LIGHT/DARK MODE-->
         <li>
@@ -69,7 +69,7 @@ let header = $(`
 <li><a href="achievements.html">Achievement</a></li>
 <li><a href="education.html">Education</a></li>
 <li><a href="https://anjali1361.github.io/my_blog/" target="_blank">Blog</a></li>
-<li><a href="https://drive.google.com/file/d/1LCpxqslcsgiKYXXmkfGSm72VEuzEaWrc/view?usp=sharing" target="_blank">My Resume</a></li>
+<li><a href="https://drive.google.com/file/d/1Kv2u4xuixG4V1ruiVdu1ujy3MrM558SM/view?usp=sharing" target="_blank">My Resume</a></li>
 
 
 <!--TOGGLE FOR LIGHT/DARK MODE-->
